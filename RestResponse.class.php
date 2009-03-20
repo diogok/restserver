0b409798eed6c2e35e00d82346ddc30f35fb96f5
Diogo SOuza da Silva <diogo@debian-acer.(none)>
2009-03-20 15:48:03 -0300

    Added phpdoc

--- RestResponse.class.php
+++ RestResponse.class.php
@@ -9,25 +9,45 @@ class RestResponse {
     private $headers ;
     private $response ;
 
+    /** 
+      * Constructor of RestServer
+      * @param RestServer $rest
+      */
     function __contruct($rest=null) {
         $this->rest = $rest ;
     }
 
+    /**
+      * Set the response to null
+      * @return RestResponse
+      */
     public function cleanResponse() {
         $this->response = null ;
         return $this ;
     }
 	
+    /**
+      * Adds a header to the response
+      * @param string $header
+      * @return RestResponse
+      */
 	public function addHeader($header) {
         $this->headers[] = $header;
         return $this  ;
 	}
 	
+    /**
+      * Clean the headers set on the response
+      * @return RestResponse
+      */
 	public function cleanHeader() {
         $this->headers = Array();
         return $this ;
 	}
 	
+    /**
+      * Show the headers
+      */
 	public function showHeader() {
         if(count($this->headers) >=1) {
             foreach($this->headers as $value) {
@@ -37,20 +57,38 @@ class RestResponse {
         return $this ;
 	}
 	
+    /**
+      * Check if headers were sent
+      * @return bool
+      */
 	public function headerSent() {
         return headers_sent();
 	}
 	
+    /**
+      * Set the response
+      * @param mixed $response
+      * @return RestResponse
+      */
 	public function setResponse($response) {
         $this->response = $response ;
         return $this ;
 	}
 	
+    /**
+      * Add a string to the response, only work if response is a string
+      * @param string $response
+      * @return RestResponse
+      */
 	public function addResponse($response) {
         $this->response .= $response ;
         return $this ;
 	}
 
+    /**
+      * Return the reponse set
+      * @return mixed $response;
+      */
     public function getResponse() {
         return $this->response ;
     }
