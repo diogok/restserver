6345b06a584f9f23de95270058056260f4321456
diogok <manifesto@manifesto.blog.br>
2009-12-02 00:01:19 -0200

    docs for api

--- example/HomeController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-class HomeController implements RestController {
-    
-    function execute(RestServer $rest) {
-        return new GenericView("home.php");
-    }
-}
-?>
--- example2/templates/home.php
+++
@@ -1,2 +0,0 @@
-<h1>Homepage</h1>
-<?php echo $rest->getParameter("user")->name ?>
