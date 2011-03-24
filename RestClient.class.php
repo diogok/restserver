a8fb88f2a4d134ba456f0e74beaf51a89da8e581
Diogo Souza da Silva <manifesto@manifesto.blog.br>
2011-03-24 17:45:35 -0300

    RestClient not failing on bad SSL

--- RestClient.class.php
+++ RestClient.class.php
@@ -26,6 +26,7 @@ class RestClient {
          curl_setopt($this->curl,CURLOPT_AUTOREFERER,true); // This make sure will follow redirects
          curl_setopt($this->curl,CURLOPT_FOLLOWLOCATION,true); // This too
          curl_setopt($this->curl,CURLOPT_HEADER,true); // This verbose option for extracting the headers
+         curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false); // This make sure it fail on SSL CA
      }
 
      /**
